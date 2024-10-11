6649acee719c5d85087fa48bd2c627d2b7966384
Huiju Kwon <00huiju00@gmail.com>
2024-10-11 17:34:18 +0900

    1011공부중

--- BE/Spring/ST_Spring/Day01_Spring_DI/Spring_Day01_DI_1_개념/src/com/ssafy/di1_의존성개념/Programmer.java
+++ BE/Spring/ST_Spring/Day01_Spring_DI/Spring_Day01_DI_1_개념/src/com/ssafy/di1_의존성개념/Programmer.java
@@ -5,12 +5,42 @@ public class Programmer {
 	//필드로 따로 가짐
 	private Desktop desktop;
 	
+	
+	//기본 생성자인 Programmer에 desktop이라는 의존성을 심어주는 것.
 	public Programmer() {
-		//프로그래머를 한명 고용했다면 묻지도 따지지도 않고 컴퓨터를 한대 새로 사줌.
+		//프로그래머를 한명 고용했다면 "묻지도 따지지도 않고" 컴퓨터를 한대 새로 사줌.
+		
+		//desktop 초기화해주는 코드 -> 강한 결합 : 다른 종류의 컴퓨터를 사용하고 싶다면
+		//programmer 클래스 자체를 수정해야함.
 		this.desktop = new Desktop();
 	}
+	
+	/**
+	    // 외부에서 Desktop 객체를 주입받는 생성자 -> 유연성이 높아짐.
+    	public Programmer(Desktop desktop) {
+        this.desktop = desktop;
+    }
+	 */
 	
 	public void coding() {
 		System.out.println(desktop.getInfo()+"으로 개발을 합니다.");
 	}
 }
+
+
+/*
+ * 기본 생성자(아무런 인자없이 호출 가능)의 역할
+ * 1. 객체를 생성했을 때, 객체를 초기화 할 수 있으며, 클래스 내에서 초기화해야 할 필드가
+ * 있는 경우 기본값을 설정할 수도 있다.
+ * 2. 자동 생성 : 클래스에 따로 생성해주지않았다면, 컴파일러가 자동생성해줌.
+ * -> 하지만 파라미터가 있는 생성자를 선언한 경우 자동생성은 없으니 유의
+ * 
+ * 매개변수가 있는 생성자를 만들어줘도 기본 생성자를 만들어주는 이유?
+ * -> 나중에 매개변수를 입력하지않고 객체를 호출했을 경우의 에러를 방지하기 위해서!
+ * 
+ *  Spring은 Reflection을 이용해서 객체를 생성한다. -> 모든 생성자를 읽어올 수 있음.
+ *  
+ * 
+ * 
+ * 
+ */
--- BE/Spring/ST_Spring/Day01_Spring_DI/Spring_Day01_DI_1_개념/src/com/ssafy/di1_의존성개념/Test.java
+++ BE/Spring/ST_Spring/Day01_Spring_DI/Spring_Day01_DI_1_개념/src/com/ssafy/di1_의존성개념/Test.java
@@ -3,8 +3,17 @@ package com.ssafy.di1_의존성개념;
 public class Test {
 	public static void main(String[] args) {
 		//프로그래머가 데스크톱에 대한 의존성을 가지고 있다.
+		//프로그래머는 데스크톱이 있어야 일을 할 수 있다 
+		// Programmer 기본 생성자에 데스크톱을 직접 생성해서 넣어줬음.
 		Programmer p = new Programmer();
 		
 		p.coding();
+		
+		
 	}
 }
+/*
+ * Class A 객체가 어떤 일을 처리하기 위해서 Class B의 객체의 도움을 받아야만 일을 처리할 수 있다
+ * = Class A(Programmer.class)는 Class B(desktop.class)에 의존한다
+ * 
+ */
